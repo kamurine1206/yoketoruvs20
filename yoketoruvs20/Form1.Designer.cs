8b162a1dde242ef51fcf12cfef607288a4f745a2
yometo <blackymt@vip.qq.com>
2020-09-10 12:20:22 +0900

    序盤

--- yoketoruvs20/Form1.Designer.cs
+++ yoketoruvs20/Form1.Designer.cs
@@ -29,12 +29,121 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.daimei = new System.Windows.Forms.Label();
+            this.START = new System.Windows.Forms.Button();
+            this.name = new System.Windows.Forms.Label();
+            this.Time = new System.Windows.Forms.Label();
+            this.Highscore = new System.Windows.Forms.Label();
+            this.zanki = new System.Windows.Forms.Label();
+            this.gameover = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // daimei
+            // 
+            this.daimei.AutoSize = true;
+            this.daimei.Font = new System.Drawing.Font("HG行書体", 50.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.daimei.ForeColor = System.Drawing.Color.DarkBlue;
+            this.daimei.Location = new System.Drawing.Point(105, 111);
+            this.daimei.Name = "daimei";
+            this.daimei.Size = new System.Drawing.Size(573, 67);
+            this.daimei.TabIndex = 0;
+            this.daimei.Text = "よけとる２０２０";
+            // 
+            // START
+            // 
+            this.START.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.START.Location = new System.Drawing.Point(265, 276);
+            this.START.Name = "START";
+            this.START.Size = new System.Drawing.Size(208, 84);
+            this.START.TabIndex = 1;
+            this.START.Text = "スタート";
+            this.START.UseVisualStyleBackColor = true;
+            // 
+            // name
+            // 
+            this.name.AutoSize = true;
+            this.name.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.name.Location = new System.Drawing.Point(244, 408);
+            this.name.Name = "name";
+            this.name.Size = new System.Drawing.Size(253, 20);
+            this.name.TabIndex = 2;
+            this.name.Text = "Copyright @ 2020 佐藤　神威";
+            // 
+            // Time
+            // 
+            this.Time.AutoSize = true;
+            this.Time.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Time.Location = new System.Drawing.Point(34, 26);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(119, 27);
+            this.Time.TabIndex = 3;
+            this.Time.Text = "TIme 100";
+            // 
+            // Highscore
+            // 
+            this.Highscore.AutoSize = true;
+            this.Highscore.Font = new System.Drawing.Font("MS UI Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Highscore.Location = new System.Drawing.Point(314, 216);
+            this.Highscore.Name = "Highscore";
+            this.Highscore.Size = new System.Drawing.Size(127, 20);
+            this.Highscore.TabIndex = 4;
+            this.Highscore.Text = "ハイスコア　100";
+            // 
+            // zanki
+            // 
+            this.zanki.AutoSize = true;
+            this.zanki.Font = new System.Drawing.Font("MS UI Gothic", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.zanki.Location = new System.Drawing.Point(640, 26);
+            this.zanki.Name = "zanki";
+            this.zanki.Size = new System.Drawing.Size(134, 27);
+            this.zanki.TabIndex = 5;
+            this.zanki.Text = "残機☆:１０";
+            // 
+            // gameover
+            // 
+            this.gameover.AutoSize = true;
+            this.gameover.Font = new System.Drawing.Font("MS UI Gothic", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.gameover.Location = new System.Drawing.Point(245, 26);
+            this.gameover.Name = "gameover";
+            this.gameover.Size = new System.Drawing.Size(252, 54);
+            this.gameover.TabIndex = 6;
+            this.gameover.Text = "Gameover";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.gameover);
+            this.Controls.Add(this.zanki);
+            this.Controls.Add(this.Highscore);
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.name);
+            this.Controls.Add(this.START);
+            this.Controls.Add(this.daimei);
+            this.Name = "Form1";
+            this.Text = "よけとる2020";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label daimei;
+        private System.Windows.Forms.Button START;
+        private System.Windows.Forms.Label name;
+        private System.Windows.Forms.Label Time;
+        private System.Windows.Forms.Label Highscore;
+        private System.Windows.Forms.Label zanki;
+        private System.Windows.Forms.Label gameover;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- yoketoruvs20/Form1.cs
+++ yoketoruvs20/Form1.cs
@@ -12,9 +12,51 @@ namespace yoketoruvs20
 {
     public partial class Form1 : Form
     {
+        enum State
+        {
+            None = -1,        //無効
+            Title,        //タイトル
+            Game,       //ゲーム
+            Gameover,    //ゲームオーバー
+            Clear,       // クリア
+        }
+        State currentState = State.None;
+        State nextState = State.Title;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(nextState != State.None)
+            {
+                initProc();
+            }
+        }
+
+        void initProc()
+        {
+            currentState = nextState;
+            nextState = State.None;
+
+            switch (currentState)
+            {
+                case State.Title;
+                    daimei.Visible = true;
+                    START.Visible = true;
+                    gameover.Visible = true;
+                    Highscore.Visible = true;
+                    Time.Visible = true;
+                    zanki.Visible = true;
+                    name.Visible = true;
+                    break;
+            }
         }
     }
 }
